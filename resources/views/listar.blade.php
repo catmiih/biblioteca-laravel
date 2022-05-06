f35aa4a9978cce7c5b151da03228439b79163995
catmiih <emilyleme.dev@gmail.com>
2022-05-06 16:49:39 -0300

    Projeto Laravel

---
+++ resources/views/listar.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+</head>
+
+<body>
+    <h1>Produtos</h1>
+    <form action="" method="post">
+
+        <label for="lblNome">Nome:</label>
+        <input type="text" name="nome" value="{{ $produto->nome }}">
+
+        <br><br>
+
+        <label for="lblValor">Valor:</label>
+        <input type="text" name="valor" value="{{ $produto->valor }}">
+
+        <br><br>
+
+        <label for="lblQuantidade">Quantidade:</label>
+        <input type="text" name="estoque" value="{{ $produto->estoque }}">
+
+        <br><br>
+
+    </form>
+
+</body>
+
+</html>
